99ef8d8a64f79a70e99c83bec678d71ce002e8a2
alonepalyer <alonepalyer@gmail.com>
2014-04-08 14:44:17 -0400

    format

--- day08-Mouse/fifo.c
+++ day08-Mouse/fifo.c
@@ -17,39 +17,39 @@ void fifo8_init(struct FIFO8 *fifo, int size, unsigned char *buf)
 int fifo8_put(struct FIFO8 *fifo, unsigned char data)
 /* put data into fifo */
 {
-	if (fifo->free == 0) 
+    if (fifo->free == 0) 
     {
-		/* no free space */
-		fifo->flags |= FLAGS_OVERRUN;
-		return -1;
-	}
-	fifo->buf[fifo->p] = data;
-	fifo->p++;
-	if (fifo->p == fifo->size) 
+        /* no free space */
+        fifo->flags |= FLAGS_OVERRUN;
+        return -1;
+    }
+    fifo->buf[fifo->p] = data;
+    fifo->p++;
+    if (fifo->p == fifo->size) 
     {
-		fifo->p = 0;
-	}
-	fifo->free--;
-	return 0;
+        fifo->p = 0;
+    }
+    fifo->free--;
+    return 0;
 }
 
 int fifo8_get(struct FIFO8 *fifo)
 /* Read data from FIFO*/
 {
-	int data;
-	if (fifo->free == fifo->size) 
+    int data;
+    if (fifo->free == fifo->size) 
     {
-		/* FIFO is empty*/
-		return -1;
-	}
-	data = fifo->buf[fifo->q];
-	fifo->q++;
-	if (fifo->q == fifo->size) 
+        /* FIFO is empty*/
+        return -1;
+    }
+    data = fifo->buf[fifo->q];
+    fifo->q++;
+    if (fifo->q == fifo->size) 
     {
-		fifo->q = 0;
-	}
-	fifo->free++;
-	return data;
+        fifo->q = 0;
+    }
+    fifo->free++;
+    return data;
 }
 
 int fifo8_status(struct FIFO8 *fifo)
