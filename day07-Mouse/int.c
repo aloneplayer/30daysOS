0365be576f12b46a1379bafd619c8f81e8772f2c
alonepalyer <alonepalyer@gmail.com>
2014-04-08 13:20:47 -0400

    active mouse

---
+++ day07-Mouse/int.c
@@ -0,0 +1,61 @@
+#include "bootpack.h"
+#include <stdio.h>
+
+void init_pic(void)
+{
+	io_out8(PIC0_IMR,  0xff  ); /* 全ての割り込みを受け付けない */
+	io_out8(PIC1_IMR,  0xff  ); /* 全ての割り込みを受け付けない */
+
+	io_out8(PIC0_ICW1, 0x11  ); /* エッジトリガモード */
+	io_out8(PIC0_ICW2, 0x20  ); /* IRQ0-7は、INT20-27で受ける */
+	io_out8(PIC0_ICW3, 1 << 2); /* PIC1はIRQ2にて接続 */
+	io_out8(PIC0_ICW4, 0x01  ); /* ノンバッファモード */
+
+	io_out8(PIC1_ICW1, 0x11  ); /* エッジトリガモード */
+	io_out8(PIC1_ICW2, 0x28  ); /* IRQ8-15は、INT28-2fで受ける */
+	io_out8(PIC1_ICW3, 2     ); /* PIC1はIRQ2にて接続 */
+	io_out8(PIC1_ICW4, 0x01  ); /* ノンバッファモード */
+
+	io_out8(PIC0_IMR,  0xfb  ); /* 11111011 PIC1以外は全て禁止 */
+	io_out8(PIC1_IMR,  0xff  ); /* 11111111 全ての割り込みを受け付けない */
+
+	return;
+}
+
+#define PORT_KEYDAT		0x0060
+struct FIFO8 keyfifo;
+
+// For keyboard
+void inthandler21(int *esp)
+{
+	unsigned char data;
+    io_out8(PIC0_OCW2, 0x61);	/*Notify IRQ-01 was handled */
+	data = io_in8(PORT_KEYDAT);
+    fifo8_put(&keyfifo, data);
+    
+    return;
+}
+
+// For mouse
+struct FIFO8 mousefifo;
+void inthandler2c(int *esp)
+{
+	unsigned char data;
+   	io_out8(PIC0_OCW2, 0x64);	/*Notify IRQ-12 (No.4 in PIC 1)was handled */
+    io_out8(PIC0_OCW2, 0x62);	/*Notify IRQ-02 was handled */
+
+    data = io_in8(PORT_KEYDAT);
+	fifo8_put(&mousefifo, data);
+}
+
+void inthandler27(int *esp)
+/* PIC0からの不完全割り込み対策 */
+/* Athlon64X2機などではチップセットの都合によりPICの初期化時にこの割り込みが1度だけおこる */
+/* この割り込み処理関数は、その割り込みに対して何もしないでやり過ごす */
+/* なぜ何もしなくていいの？
+	→  この割り込みはPIC初期化時の電気的なノイズによって発生したものなので、
+		まじめに何か処理してやる必要がない。									*/
+{
+	io_out8(PIC0_OCW2, 0x67); /* IRQ-07受付完了をPICに通知 */
+	return;
+}
---
+++ day07-Mouse/readme.txt
@@ -0,0 +1,17 @@
+New-----------
+Add mosue control into int.c
+
+
+
+Done-----------
+
+
+Using FIFO : add fifo.c
+Modify Makefile
+
+
+Slipt files 
+
+Using general rules in Makefile
+
+Using bootpack.h
