5416decf8d788f039cc0bfbb0bd5e09a9f1ad28f
alonepalyer <alonepalyer@gmail.com>
2014-04-09 16:09:48 -0400

    sheet management

---
+++ day10-2-sheet/int.c
@@ -0,0 +1,35 @@
+#include "bootpack.h"
+#include <stdio.h>
+
+void init_pic(void)
+{
+	io_out8(PIC0_IMR,  0xff  ); /* 全ての割り込みを受け付けない */
+	io_out8(PIC1_IMR,  0xff  ); /* 全ての割り込みを受け付けない */
+
+	io_out8(PIC0_ICW1, 0x11  ); /* エッジトリガモード */
+	io_out8(PIC0_ICW2, 0x20  ); /* IRQ0-7は、INT20-27で受ける */
+	io_out8(PIC0_ICW3, 1 << 2); /* PIC1はIRQ2にて接続 */
+	io_out8(PIC0_ICW4, 0x01  ); /* ノンバッファモード */
+
+	io_out8(PIC1_ICW1, 0x11  ); /* エッジトリガモード */
+	io_out8(PIC1_ICW2, 0x28  ); /* IRQ8-15は、INT28-2fで受ける */
+	io_out8(PIC1_ICW3, 2     ); /* PIC1はIRQ2にて接続 */
+	io_out8(PIC1_ICW4, 0x01  ); /* ノンバッファモード */
+
+	io_out8(PIC0_IMR,  0xfb  ); /* 11111011 PIC1以外は全て禁止 */
+	io_out8(PIC1_IMR,  0xff  ); /* 11111111 全ての割り込みを受け付けない */
+
+	return;
+}
+
+void inthandler27(int *esp)
+/* PIC0からの不完全割り込み対策 */
+/* Athlon64X2機などではチップセットの都合によりPICの初期化時にこの割り込みが1度だけおこる */
+/* この割り込み処理関数は、その割り込みに対して何もしないでやり過ごす */
+/* なぜ何もしなくていいの？
+	→  この割り込みはPIC初期化時の電気的なノイズによって発生したものなので、
+		まじめに何か処理してやる必要がない。									*/
+{
+	io_out8(PIC0_OCW2, 0x67); /* IRQ-07受付完了をPICに通知 */
+	return;
+}
---
+++ day10-2-sheet/readme.txt
@@ -0,0 +1,38 @@
+New-----------
+Layer management
+	Add sheet.c
+        Modify Makefile
+
+Done-----------
+
+Refactor: add memory.c 
+          Update Makefile, bootpack.h
+
+
+Memory Management
+
+
+Get memeory size
+
+Refactor: add mouse.c and keyboard.c Modify Makefile
+
+
+Using new data struct  MOUSE_DEC 
+Parse mouse data to X, Y and Button
+
+
+Read mouse data and reflactor
+
+
+Add mosue control into int.c
+
+
+Using FIFO : add fifo.c
+Modify Makefile
+
+
+Slipt files 
+
+Using general rules in Makefile
+
+Using bootpack.h
