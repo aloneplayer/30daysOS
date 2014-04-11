f4f831d58c7535bdf165b31afc1b3597d22be0bd
alonepalyer <alonepalyer@gmail.com>
2014-04-11 09:31:09 -0400

    count++

---
+++ day12-1-PIT/debug.c
@@ -0,0 +1,9 @@
+#include "bootpack.h"
+
+void debug_print(int x, int y, unsigned char *s)
+{
+    struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+	putfonts8_asc(binfo->vram, binfo->scrnx, x, y, COL8_FF0000, s);
+
+	return;
+}
